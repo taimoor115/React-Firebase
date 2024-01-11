88f1c383dec3a1967f078dbe7438bdc6d614583e
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-01-11 22:24:32 +0500

    Adding Firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { getDatabase, ref, set } from "firebase/database";
+import app from "./firebase";
 
-function App() {
-  const [count, setCount] = useState(0)
+const db = getDatabase(app);
 
+const App = () => {
+  const putData = () => {
+    set(ref(db, "users/taimoor"), {
+      id: 1,
+      name: "Taimoor Hussain",
+      age: 21,
+    });
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Firebase</h1>
+      <button onClick={putData}>Put Data</button>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAtAXQVCh6XYjgxG_O0aVDr_UxsT2mdHOs",
+  authDomain: "fir-499fc.firebaseapp.com",
+  projectId: "fir-499fc",
+  storageBucket: "fir-499fc.appspot.com",
+  messagingSenderId: "722387737203",
+  appId: "1:722387737203:web:ed4bfb03edbfd522a92806",
+  databaseURL: "https://fir-499fc-default-rtdb.firebaseio.com",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
