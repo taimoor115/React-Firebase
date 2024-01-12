727e8080ba0aa9056fd7aaa8301633d561e11632
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-01-13 00:24:59 +0500

    Logged in

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
-import Github from "./Components/Github";
-
-// We need to set up the client key and secret key
-// Firstly go to the github setting then go to the github developer option and create the new
-// app
-
+import SignIn from "./Components/Sign-in";
+import SignUp from "./Components/SignUp";
+import { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import app from "./firebase";
+const auth = getAuth(app);
 const App = () => {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // you are logged in
+        setUser(user);
+        console.log("Login" + user);
+      } else {
+        // your are logged out
+        console.log("Logged out");
+        setUser(null);
+      }
+    });
+  }, []);
+  if (user === null) {
+    return (
+      <div>
+        <SignUp />
+        <SignIn />
+      </div>
+    );
+  }
+
   return (
     <div>
-      <Github />
+      <h1>Your are login with {user.email}</h1>
+      <button onClick={() => signOut(auth)}>Logout</button>
     </div>
   );
 };
