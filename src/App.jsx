c35ab891869d2fa1079e6ccf5e86f5a1bc72c8c4
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-01-13 13:47:30 +0500

    Firestore functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,76 @@
-import SignIn from "./Components/Sign-in";
-import SignUp from "./Components/SignUp";
-import { useEffect, useState } from "react";
-import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import {
+  getFirestore,
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  query,
+  where,
+  getDocs,
+  updateDoc,
+} from "firebase/firestore";
 import app from "./firebase";
-const auth = getAuth(app);
+
+const fireStore = getFirestore(app);
+
 const App = () => {
-  const [user, setUser] = useState(null);
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // you are logged in
-        setUser(user);
-        console.log("Login" + user);
-      } else {
-        // your are logged out
-        console.log("Logged out");
-        setUser(null);
-      }
+  // Write data in Firestore
+  const writeData = async () => {
+    const result = await addDoc(collection(fireStore, "cities"), {
+      name: "Lahore",
+      pinCode: 1234,
+      lat: 123,
+      long: 12,
     });
-  }, []);
-  if (user === null) {
-    return (
-      <div>
-        <SignUp />
-        <SignIn />
-      </div>
+
+    console.log("Result", result);
+  };
+  const writeSubData = async () => {
+    const subResult = await addDoc(
+      collection(fireStore, "cities/MISYTLoANWFIqtFsbxsK/places"),
+      {
+        place: "Harbans Pura",
+        description: "Town",
+        Date: Date.now(),
+      }
     );
-  }
+    console.log("Result", subResult);
+  };
+
+  // Read Data in Firestore
+
+  const getDocument = async () => {
+    const ref = doc(fireStore, "cities", "MISYTLoANWFIqtFsbxsK");
+    const snap = await getDoc(ref);
+
+    console.log("Result", snap.data());
+  };
+
+  // Query Data in Firestore
+  const getDocumentsByQuery = async () => {
+    const refCollection = collection(fireStore, "users");
+    const q = query(refCollection, where("isMale", "==", true));
+
+    const snapShot = await getDocs(q);
+
+    snapShot.forEach((query) => console.log(query.data()));
+  };
+
+  // Update Data in Firestore
+  const updateData = async () => {
+    const docRef = doc(fireStore, "cities", "MISYTLoANWFIqtFsbxsK");
+    await updateDoc(docRef, {
+      name: "Karachi",
+    });
+  };
 
   return (
     <div>
-      <h1>Your are login with {user.email}</h1>
-      <button onClick={() => signOut(auth)}>Logout</button>
+      <button onClick={writeData}>Put Data</button>
+      <button onClick={writeSubData}>Put SubData</button>
+      <button onClick={getDocument}> Read Data</button>
+      <button onClick={getDocumentsByQuery}> Read Data by Query</button>
+      <button onClick={updateData}>Update City</button>
     </div>
   );
 };
